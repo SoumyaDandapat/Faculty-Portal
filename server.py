9662025a63ad4fa792bfd40ff9143021f83560bb
nikhil1091 <2017csb1091@iitrpr.ac.in>
2019-11-04 12:56:34 +0530

    added templated files with bootstrap

--- server.py
+++ server.py
@@ -1,22 +1,43 @@
+import os
 
-from flask import Flask, redirect, url_for,request
+from flask import Flask, redirect, request, url_for
+from flask.helpers import send_from_directory
+from flask.templating import render_template
 
 app = Flask(__name__)
 
-@app.route('/1')
-def hello_world1():
-    return 'Hello World 1!!'
+@app.route("/authorize", methods=['POST'])
+def authorize():
+    input=request.form.to_dict()
+    return "true"
 
-@app.route('/login',methods=['POST','GET'])
-def login():
-    #print(request)
-    return "hello "+ request.form['a']+" "+request.form['b']
+@app.route("/register",methods=['POST','GET'])
+def register_page():
+    if(request.method=="GET"):
+        return render_template('register.html')
+    else:
+        input=request.form.to_dict()
+        
 
-@app.route('/2')
-def hello_world2():
-    return redirect(url_for('hello_world1'))
+@app.route("/navhandler!<tog>")
+def navigator(tog):
+    
+    if(tog=="register"):
+        return redirect(url_for("register_page"))
+    elif(tog=="logout"):
+        return redirect(url_for("logout_session"))
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
-    app.run(debug = True)
+@app.route('/login',methods=["GET","POST"])
+def login_page():
+    if(request.method=="GET"):
+        return render_template('login_page.html')
+    else:
+        input=request.form.to_dict()
+        return input
+
+
+
+#os.system("google-chrome /home/nikhil/Desktop/cs301/project/Faculty-Portal/login_page.html")
+app.debug = True
+app.run()
+app.run(debug = True)

9b079c0c5c32e42b11844028c85a9b4344b9d5b4
SoumyaDandapat <45857117+SoumyaDandapat@users.noreply.github.com>
2019-11-12 23:49:18 +0530

    Update schema.sql
    hi

--- schema.sql
+++ schema.sql
@@ -1,7 +1,17 @@
+create table id(
+    id int default 0
+);
+
 create table employees(
     eid int primary key,
-    employee_name varchar(30),
-    pwd varchar(30),
+    employee_name varchar(30) not null,
+    pwd varchar(30) not null,
     dept varchar(5),
     leaves_left int 
+);
+
+create table hod(
+    hod_id int,
+    dept_name varchar(5)
+    foreign key(hod_id) references employees(eid) 
 );

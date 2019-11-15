8bb7560ccc50319b2f5ef6db2453354828dc088f
SoumyaDandapat <45857117+SoumyaDandapat@users.noreply.github.com>
2019-11-15 22:11:45 +0530

    deleting redundant files

--- database/schema.sql
+++ database/schema.sql
@@ -1,14 +1,10 @@
-create table id(
-    id int default 0
-);
-
-create table leave_id(
-    leave_id int default 0
+create table const(
+    id int default 0,
+	leave_id int default 0
 );
 
 create table employees(
     eid int primary key,
-    employee_name varchar(30) not null,
     pwd varchar(30) not null,
     dept varchar(5),
     leaves_left int 
@@ -17,46 +13,30 @@ create table employees(
 create table hod(
     hod_id int,
     dept_name varchar(5),
+	start_time time,
+    end_time time,
     foreign key(hod_id) references employees(eid) 
 );
 
 create table dean(
     dean_id int,
     dean_type varchar(15),
+	start_time time,
+    end_time time,
     foreign key(dean_id) references employees(eid)
-);
-
-create table hod_status(
-    hod_dept_name varchar(15),
-    start_time time,
-    end_time time 
-);
-
-create table dean_status(
-    dtype varchar(15),
-    start_time time,
-    end_time time
 );
 
 create table leave_application(
     leave_id int primary key,
-    leave_status varchar(1) default "n",
-)
-create table applicant(
-    leave_id int,
-    applicant_id int,
+	applicant_id int,
     reason varchar(200),
-    day int,
-    foreign key(leave_id) references leave_application(leave_id),
-    foreign key(applicant_id) references employees(eid)
-);
-
-create table administrate(
-    administrate_id int,
+    days int,
+	position int,
+	administrate_id int,
     comment varchar(200),
-    leave_id int,
-    foreign key(administrate_id) references employees(eid),
-    foreign key(leave_id) references leave_application(leave_id)
+    leave_status varchar(1) default "n",
+	foreign key(administrate_id) references employees(eid),
+	foreign key(applicant_id) references employees(eid)
 );
 
 create table ranks(
@@ -76,6 +56,8 @@ create table leave_database(
 
 create table director(
     director_id int,
+	start_time time,
+    end_time time,
     foreign key(director_id) references employees(eid)
 );
 
